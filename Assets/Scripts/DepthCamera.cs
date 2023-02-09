686d040ddfafc68ae801133098e4ed8194fc77b5
Ricardo Franco <ricardosf195@gmail.com>
2023-02-09 16:03:51 +0000

    Ros-Messages Added

--- Assets/Scripts/DepthCamera.cs
+++ Assets/Scripts/DepthCamera.cs
@@ -1,12 +1,18 @@
 using UnityEngine;
 using UnityEngine.Rendering;
+using Unity.Robotics.ROSTCPConnector;
+using RosMessageTypes.DepthCamera;
 
 public class DepthCamera : MonoBehaviour
 {
     [Header("Shader Setup")]
     public Shader uberReplacementShader;
 
+    public Shader depthShader;
     private Camera camera;
+
+    ROSConnection ros;
+    private string cameraInfoTopic = "sensor_msgs";
 
     void Start()
     {
@@ -16,10 +22,29 @@ public class DepthCamera : MonoBehaviour
 
         camera = GetComponent<Camera>();
         camera.targetDisplay = 1;
-        
+
         SetupCameraWithReplacementShader(uberReplacementShader, Color.white);
+        publishCameraIntrisics();
     }
 
+
+    private void publishCameraIntrisics()
+    {
+        // camera Intrinsics 
+        Matrix4x4 projection = camera.projectionMatrix;
+        CameraInfoMsg cameraIntrinsics = new CameraInfoMsg(
+            Mathf.Atan(1 / projection[1, 1]) * 2 * Mathf.Rad2Deg,
+            projection[1, 1] / projection[0, 0],
+            camera.nearClipPlane,
+            camera.farClipPlane
+        );
+
+
+        // publishing camera Intrinsics to sensor_msgs/CameraInfo ROS channel
+        ros = ROSConnection.GetOrCreateInstance();
+        ros.RegisterPublisher<CameraInfoMsg>(cameraInfoTopic);
+        ros.Publish(cameraInfoTopic, cameraIntrinsics);
+    }
 
 
     private void SetupCameraWithReplacementShader(Shader shader, Color clearColor)
